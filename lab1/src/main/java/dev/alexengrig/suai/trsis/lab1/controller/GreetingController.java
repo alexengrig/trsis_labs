daa8b08b6e4646e19d91b75d31325a623fb4ed60
Grig Alex <alexengrigdev@gmail.com>
2020-01-05 21:51:41 +0300

    Add GreetingController.java

---
+++ lab1/src/main/java/dev/alexengrig/suai/trsis/lab1/controller/GreetingController.java
@@ -0,0 +1,15 @@
+package dev.alexengrig.suai.trsis.lab1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+    @GetMapping
+    public String greeting(@RequestParam(value = "name", defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
