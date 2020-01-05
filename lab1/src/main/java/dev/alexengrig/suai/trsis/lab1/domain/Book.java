37782e454d24bfb303a259df3231245d9d67019d
Grig Alex <alexengrigdev@gmail.com>
2020-01-05 21:50:56 +0300

    Add Book.java

---
+++ lab1/src/main/java/dev/alexengrig/suai/trsis/lab1/domain/Book.java
@@ -0,0 +1,25 @@
+package dev.alexengrig.suai.trsis.lab1.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.Year;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String author;
+    private Year year;
+}
