c57ef8809591f7267f99ad21f94b73a587fedfb0
Grig Alex <alexengrigdev@gmail.com>
2020-01-13 19:05:01 +0300

    Add MVCConfiguration.java with static resource handler

---
+++ lab1/src/main/java/dev/alexengrig/suai/trsis/lab1/configuation/MVCConfiguration.java
@@ -0,0 +1,14 @@
+package dev.alexengrig.suai.trsis.lab1.configuation;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MVCConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
+}
