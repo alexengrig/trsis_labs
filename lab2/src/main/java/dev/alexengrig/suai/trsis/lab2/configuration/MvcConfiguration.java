3885ed672a1f9f8659f1213831f19fcb32550a0d
Grig Alex <alexengrigdev@gmail.com>
2020-01-16 19:24:14 +0300

    Allow all methods

--- lab2/src/main/java/dev/alexengrig/suai/trsis/lab2/configuration/MvcConfiguration.java
+++ lab2/src/main/java/dev/alexengrig/suai/trsis/lab2/configuration/MvcConfiguration.java
@@ -13,7 +13,8 @@ public class MvcConfiguration {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry.addMapping("/**")
+                        .allowedMethods("*");
             }
         };
     }
