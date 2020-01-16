9e5cf22e22dd0b1bfaca9f0f803706af0b84c428
Grig Alex <alexengrigdev@gmail.com>
2020-01-16 21:35:51 +0300

    Add SecurityConfiguration.java

---
+++ lab4/src/main/java/dev/alexengrig/suai/trsis/lab4/configuration/SecurityConfiguration.java
@@ -0,0 +1,36 @@
+package dev.alexengrig.suai.trsis.lab4.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("admin").roles("ADMIN")
+                .and()
+                .withUser("user").password("user").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/books/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/books").authenticated()
+                .antMatchers(HttpMethod.PUT, "/books/**").authenticated()
+                .antMatchers(HttpMethod.DELETE, "/books/**").hasRole("ADMIN")
+                .and()
+                .csrf().disable()
+                .formLogin().disable();
+    }
+}
